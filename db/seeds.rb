a1125173e65fc1e5bb17aa1c722c0dfc39b3d2c8
paulhurrle <paul_hurrle@yahoo.com>
2017-09-10 09:49:43 -0700

    complete assignment-16

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require 'random_data'
 
+Post.find_or_create_by!(title: "This is my unique post title", body: "This is my unique post body")
+
+Comment.find_or_create_by!(body: "This is my unique comment body", post_id: 51)
+
 50.times do
     Post.create!(
         title: RandomData.random_sentence,
