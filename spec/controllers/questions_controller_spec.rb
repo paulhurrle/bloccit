79d9ef2e390d7405a2c3f3c92c8905586eabdf8e
paulhurrle <paul_hurrle@yahoo.com>
2017-09-12 18:45:08 -0700

    edit assignment-20, add additional testing for edit, update, and destroy

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -64,4 +64,64 @@ RSpec.describe QuestionsController, type: :controller do
             expect(response).to redirect_to Question.last
         end
     end
+
+    describe "GET #edit" do
+      it "returns http success" do
+        get :edit, {id: my_question.id}
+        expect(response).to have_http_status(:success)
+      end
+
+      it "renders the #edit view" do
+          get :edit, {id: my_question.id}
+          expect(response).to render_template :edit
+      end
+
+      it "assigns question to be updated to @question" do
+          get :edit, {id: my_question.id}
+
+          question_instance = assigns(:question)
+          expect(question_instance.id).to eq my_question.id
+          expect(question_instance.title).to eq my_question.title
+          expect(question_instance.body).to eq my_question.body
+          expect(question_instance.resolved).to eq my_question.resolved
+      end
+    end
+
+    describe "PUT update" do
+        it "updates question with expected attributes" do
+            new_title = RandomData.random_sentence
+            new_body = RandomData.random_paragraph
+            new_resolved = true
+
+            put :update, id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved}
+
+            updated_question = assigns(:question)
+            expect(updated_question.id).to eq my_question.id
+            expect(updated_question.title).to eq new_title
+            expect(updated_question.body).to eq new_body
+            expect(updated_question.resolved).to eq new_resolved
+        end
+
+        it "redirects to the updated question" do
+            new_title = RandomData.random_sentence
+            new_body = RandomData.random_paragraph
+            new_resolved = true
+
+            put :update, id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved}
+            expect(response).to redirect_to my_question
+        end
+    end
+
+    describe "DELETE destroy" do
+        it "deletes the question" do
+            delete :destroy, {id: my_question.id}
+            count = Question.where({id: my_question.id}).size
+            expect(count).to eq 0
+        end
+
+        it "redirects to question index" do
+            delete :destroy, {id: my_question.id}
+            expect(response).to redirect_to questions_path
+        end
+    end
 end
